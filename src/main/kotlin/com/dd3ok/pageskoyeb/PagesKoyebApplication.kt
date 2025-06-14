0e234b3fa4ac40ea7ac747ce4de9edc9529274d7
dd3ok <hwick@nate.com>
2025-06-14 15:29:12 +0900

    init

---
+++ src/main/kotlin/com/dd3ok/pageskoyeb/PagesKoyebApplication.kt
@@ -0,0 +1,11 @@
+package com.dd3ok.pageskoyeb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PagesKoyebApplication
+
+fun main(args: Array<String>) {
+    runApplication<PagesKoyebApplication>(*args)
+}
---
+++ src/test/kotlin/com/dd3ok/pageskoyeb/PagesKoyebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dd3ok.pageskoyeb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PagesKoyebApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
